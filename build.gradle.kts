572fed71c6e2f47c1d3be7144eadcfa7f9921a29
HassanAyman <hassan3ayman@gmail.com>
2024-01-29 13:25:22 +0200

    add dependencies and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21" apply false
 }
